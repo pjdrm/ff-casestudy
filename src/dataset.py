ee72ccfecab460514b5241a1534b10ebd79541b1
pjdrm <pjdrm@ist.utl.pt>
2019-08-28 16:36:48 +0100

    Adding dataset class prototype.

---
+++ src/dataset.py
@@ -0,0 +1,17 @@
+'''
+Created on Aug 28, 2019
+
+@author: pjdrm
+'''
+
+class Data(object):
+    '''
+    classdocs
+    '''
+
+
+    def __init__(self, params):
+        '''
+        Constructor
+        '''
+        
